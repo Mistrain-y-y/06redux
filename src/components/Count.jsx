41d986690737c20cf1eec875de2019cac48f1ce9
mistrain <1053401058@qq.com>
2021-04-05 13:28:42 +0800

    4.5 first commit

--- src/components/Count.jsx
+++ src/components/Count.jsx
@@ -1,9 +1,5 @@
+// UI 组件
 import React, { Component } from 'react'
-import {
-  createIncrementAction,
-  createDecrementAction,
-  createAsyncIncrement
-} from '../redux/count_action'
 import store from '../redux/store'
 
 export default class Count extends Component {
@@ -15,31 +11,24 @@ export default class Count extends Component {
   }
   increment = () => {
     const { value } = this.optionValue
-    store.dispatch(createIncrementAction(value * 1))
   }
   decrement = () => {
     const { value } = this.optionValue
-    store.dispatch(createDecrementAction(value * 1))
   }
   incrementIfOdd = () => {
     const { value } = this.optionValue
     const count = store.getState()
     if (count % 2 === 1) {
-      store.dispatch(createIncrementAction(value * 1))
+
     }
   }
   incrementAsync = () => {
     const { value } = this.optionValue
-    // const timer = setTimeout(() => {
-    //   store.dispatch(createIncrementAction(value * 1))
-    //   clearTimeout(timer)
-    // }, 1000)// 同步action写法
-    store.dispatch(createAsyncIncrement(value * 1, 1000))// 异步action写法
   }
   render() {
     return (
       <div>
-        <h1>当前求和为 {store.getState()}</h1>
+        <h1>当前求和为 0</h1>
         <select ref={val => this.optionValue = val}>
           <option value={1}>1</option>
           <option value={2}>2</option>
